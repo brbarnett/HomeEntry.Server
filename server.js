2ede7344fca15d42873627acb1e5331a0f11cb14
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-04-10 08:57:53 -0500

    Auth method

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+app.use( bodyParser.json());
+
+app.post('/api/entry/authenticate', function(req, res) {
+  res.send(req.body);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
