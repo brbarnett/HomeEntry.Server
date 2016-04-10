78ac757eaf644167f362e3b47cc6baf81461a913
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-04-10 09:17:01 -0500

    Authenticating a single code

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 var app = express();
 app.use( bodyParser.json());
 
+var allowed = [
+    '0x829770c2' // myfare
+    ];
+
 app.post('/api/entry/authenticate', function(req, res) {
-  res.send(req.body);
+  var uid = req.body.uid;
+  
+  res.send(_.includes(allowed, uid));
 });
 
 app.listen(3000, function () {
